c4f86e8749df03301b6e90eaab5051ec645ad8b3
Lukarrio1 <tomennis1997@gmail.com>
2019-05-22 17:52:23 -0500

    landing page added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
-<script src="{{asset('js/mdb.min.js')}}"></script>
+{{-- <script src="{{asset('js/mdb.min.js')}}"></script> --}}
 <script src="{{asset('iziToast.js')}}"></script>
   <script src="{{asset('js/index.js')}}"></script>
   <script src="{{ asset('js/semantic.min.js') }}"></script>
